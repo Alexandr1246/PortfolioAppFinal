67b0ac1c8a54ba4d05c4680887f8a1a5a93ba69a
Alex <alex.sh1246@gmail.com>
2022-04-15 23:08:58 +0300

    final version

--- app/src/main/java/com/example/appdemo/MainActivity.kt
+++ app/src/main/java/com/example/appdemo/MainActivity.kt
@@ -16,67 +16,57 @@ import com.google.firebase.ktx.Firebase
 class MainActivity : AppCompatActivity() {
 
     private val signInLauncher =
-        registerForActivityResult(      // Создает обьект авторизации екрана
+        registerForActivityResult(
             FirebaseAuthUIActivityResultContract()
         ) { res ->
-            this.onSignInResult(res)                                 // запускает екран регистрации
+            this.onSignInResult(res)
         }
 
-    private lateinit var database: DatabaseReference            // создали обьект для записи в базу данных
-
+    private lateinit var database: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) // <== старт приложение указывается с какого активини программе начать рабоату
+        setContentView(R.layout.activity_main)
         Log.d("testLog", "in onCreate")
 
-        Log.d("testLogs", "RegistrationActivity start registration")
-        database = Firebase.database.reference                  // Инициализация базы данных
+        database = Firebase.database.reference
 
         val providers = arrayListOf(
             AuthUI.IdpConfig.EmailBuilder().build()
-        )            // список модулей которые мы будем использовать для авторизации
+        )
 
         val signInIntent =
-            // пишем логику для сбора данных, создаем поля для заполнения при ригистрации
+
             AuthUI.getInstance()
                 .createSignInIntentBuilder()
                 .setAvailableProviders(providers)
                 .build()
-        signInLauncher.launch(signInIntent)                     // запускает екран регистрации в auth
+        signInLauncher.launch(signInIntent)
 
     }
 
     private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
         val response =
-            result.idpResponse                               // принимает результат с екрана FireBase auth
-        if (result.resultCode == RESULT_OK) {                       // если результат регистрации успешный
+            result.idpResponse
+        if (result.resultCode == RESULT_OK) {
             Log.d("testLogs", "RegistrationActivity registration success ${response?.email}")
-            // Successfully signed in
             val authUser =
-                FirebaseAuth.getInstance().currentUser   // тогда создаем пользователя в firebase auth
-            authUser?.let {             //если он не 0 (существует) сохраняем его в real time database
-                val email = it.email.toString() // извлекаем email нашего пользователя
-                val uid = it.uid // извлекаем uid нашего пользователя
-                val firebaseUser = User(email, uid) // создаем новый обьект User в database с параметрами email и uid
+                FirebaseAuth.getInstance().currentUser
+            authUser?.let {
+                val email = it.email.toString()
+                val uid = it.uid
+                val firebaseUser = User(email, uid)
                 Log.d("testLogs", "RegistrationActivity firebaseUser $firebaseUser")
                 database.child("users").child(uid)
-                    .setValue(firebaseUser)       // сохраняем нашего пользователя в FireBase RealTime
+                    .setValue(firebaseUser)
+                val intentToAnotherScreen = Intent(this, MoviesActivity::class.java)
+                startActivity(intentToAnotherScreen)
 
-                val intentToAnotherScreen = Intent(
-                    this@MainActivity,
-                    MoviesActivity::class.java
-                ) // <== отправляем пользователя на другой экран
-                startActivity(intentToAnotherScreen) // <== отправляем пользователя на другой экран
-                //Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show() // <== появляющееся сообщение о нажатии на кнопку
+
             }
 
-        } else {    // если результат не "ок" должны обработать ошибку
+        } else {
             Log.d("testLogs", "RegistrationActivity registration failure")
-            Toast.makeText(this@MainActivity, "Error with registration", Toast.LENGTH_SHORT).show() // <== появляющееся сообщение о нажатии на кнопку
-            // Sign in failed. If response is null the user canceled the
-            // sign-in flow using the back button. Otherwise check
-            // response.getError().getErrorCode() and handle the error.
-            // ...
+            Toast.makeText(this@MainActivity, "Error with registration", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/example/appdemo/MoviesActivity.kt
+++ app/src/main/java/com/example/appdemo/MoviesActivity.kt
@@ -1,63 +1,54 @@
 package com.example.appdemo
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.appdemo.Constants.API_KEY
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MoviesActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {        // вызов екрана регистрации
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movies)
         Log.d("testLog", "MoviesActivity start activity")
-        // getting the recyclerview by its id
-        val recyclerview = findViewById<RecyclerView>(R.id.recyclerview) //создаем список
-
-        // this creates a vertical layout Manager
-        recyclerview.layoutManager = GridLayoutManager(this, 2) // указывает что список будет вертикальным
-
-        // ArrayList of class ItemsViewModel
+        val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
+        recyclerview.layoutManager = GridLayoutManager(this, 2)
         val data = ArrayList<ItemsViewModel>()
-
-        // This loop will create 20 Views containing
-        // the image with the count of view
         for (i in 1..20) {
-            data.add(ItemsViewModel(R.drawable.common_full_open_on_phone, "Item " + i))
+            data.add(ItemsViewModel(R.drawable.common_full_open_on_phone, "Item+i"))
         }
+        val apiInterface = ApiInterface.create().getMovies(API_KEY)
 
-
-
-        val apiInterface = ApiInterface.create().getMovies("972ed1f09cd44308501949c0f3763a1f")
-
-        //apiInterface.enqueue( Callback<List<Movie>>())
-        apiInterface.enqueue( object : Callback<Movies> {
+        apiInterface.enqueue(object : Callback<Movies>, CustomAdapter.ItemClickListener {
             override fun onResponse(call: Call<Movies>?, response: Response<Movies>?) {
-            Log.d("testLog", "OnResponse Success ${response?.body()?.results}")
-
-                // This will pass the ArrayList to our Adapter
-                val adapter = CustomAdapter(response?.body()?.results)
-
-                // Setting the Adapter with the recyclerview
-                recyclerview.adapter = adapter // передаем данные в список
-
-//                if(response?.body() != null)
-//                    recyclerAdapter.setMovieListItems(response.body()!!)
+                Log.d(
+                    "testLog",
+                    "OnResponse Success ${call.toString()} ${response?.body()?.results?.size}"
+                )
+                val adapter = CustomAdapter(response?.body()?.results, this)
+                recyclerview.adapter = adapter
             }
 
             override fun onFailure(call: Call<Movies>?, t: Throwable?) {
                 Log.d("testLog", "onFailure : ${t?.message}")
             }
+
+            override fun onItemClick(id: Int) {
+                val intent = Intent(this@MoviesActivity, MoviesDetailsActivity::class.java)
+                intent.putExtra("id", id)
+                startActivity(intent)
+            }
         })
     }
 
-    override fun onBackPressed() { // отвечает за то чтоб приложение закрывалось при нажатии кнопки назад
+    override fun onBackPressed() {
         super.onBackPressed()
         this.finishAffinity()
         Log.d("testLog", "MoviesActivity application been closed")
